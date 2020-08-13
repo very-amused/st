521e5b2d8c06a1dbc0954cc59daba80ab391d963
very-amused <very-amused@pm.me>
2020-08-13 18:35:58 -0400

    Add ligature support

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:size=14:antialias=true:autohint=true";
+static char *font = "Fira Code:size=12:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
