ed1ddc1c14dc0194f5811a2a5b97e0465656ee19
very-amused <very-amused@pm.me>
2023-06-08 16:11:57 -0400

    Switch to Fira Code Nerd Font Mono

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:size=12:antialias=true:autohint=true";
-static char *font2[] = { "Nerd Fonts Symbols:size=12:antialias=true:autohint=true" };
+static char *font = "Fira Code Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char *font2[] = {};
 static int borderpx = 10;
 
 /*
