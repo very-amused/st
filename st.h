35a8257d6eeaa56a8ed0c730875dab2da90131b0
very-amused <very-amused@pm.me>
2024-05-31 00:17:36 -0400

    Revert scrollback-ringbuffer patch (0.9.2)
    The ringbuffer implementation was buggy in my experience,
    so I'm going back to the standard scrollback implementation

--- st.h
+++ st.h
@@ -20,7 +20,6 @@
 
 #define TRUECOLOR(r,g,b)	(1 << 24 | (r) << 16 | (g) << 8 | (b))
 #define IS_TRUECOL(x)		(1 << 24 & (x))
-#define HISTSIZE            2000
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
