f0b9a9e4ee77ba3cf4a830b12968e3400021fbe9
very-amused <very-amused@pm.me>
2025-02-14 21:32:22 -0500

    Switch from 11pt to 10pt terminal font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font:size=11:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font:size=10:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
